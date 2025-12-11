699215ed9efe8b048a1cb4ad0c7ec49091266a21
Mijo Šepović <sepovic.mijo@gmail.com>
2025-12-11 21:17:16 +0100

    Refactor LoginPage for UI update

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,42 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:projectflow/pages/home_page.dart';
 import 'package:supabase_auth_ui/supabase_auth_ui.dart';
-import 'home_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: FlutterLogo(), //put as a placeholder until a final logo is designed
-        title: const Text('Login Page'),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('ProjectFlow', style: Theme.of(context).textTheme.headlineMedium),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 200),
-              child: SupaEmailAuth(
-                onSignInComplete: (response) {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
-                  );
-                }, 
-                onSignUpComplete: (response) {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
-                  );
-                }
+        child: Container(
+          margin: EdgeInsets.all(45),
+          height: 500,
+          width: 450,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: const Color.fromARGB(255, 135, 170, 184),
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 50,),
+              Text('ProjectFlow',
+                  style: TextStyle(
+                    fontSize: 40
+                  ),
               ),
-            )
-          ],
-        ),
+              SizedBox(height: 50,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SupaEmailAuth(
+                      onSignInComplete: (response) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => const HomePage()),
+                        );
+                      }, 
+                      onSignUpComplete: (response) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => const HomePage()),
+                        ); },
+                        ),
+              ),
+            ],
+          ),
       ),
-    );
+    ),
+   );
   }
 }
